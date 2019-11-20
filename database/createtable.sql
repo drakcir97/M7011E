39ec2ea3d7e666a11c6495e7f3b02a5e0aff4ee6
Ubuntu <ubuntu@ip-172-31-84-239.ec2.internal>
2019-11-20 12:34:29 +0000

    Updates database

--- database/createtable.sql
+++ database/createtable.sql
@@ -1,60 +1,69 @@
 USE miri;
 
 CREATE TABLE IF NOT EXISTS location (
-        id INT NOT NULL AUTO_INCREMENT,
-        name CHAR(255),
+        id int NOT NULL AUTO_INCREMENT,
+        name char(255) UNIQUE,
         PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS household (
-	id INT NOT NULL AUTO_INCREMENT,
-	locationid INT NOT NULL,
-	powerusageid INT NOT NULL,
-	powergeneratedid INT NOT NULL,
-	housetype CHAR(255),
+	id int NOT NULL AUTO_INCREMENT,
+	locationid int NOT NULL,
+	powerusageid int NOT NULL,
+	powergeneratedid int NOT NULL,
+	housetype char(255),
 	PRIMARY KEY (id),
 	FOREIGN KEY (locationid) REFERENCES location(id)
 );
 
 CREATE TABLE IF NOT EXISTS user (
-        id INT NOT NULL AUTO_INCREMENT,
-        householdid INT NOT NULL,
-        PRIMARY KEY (userid),
-        FOREIGN KEY (householdid) REFERENCES household(id),
+        id int NOT NULL AUTO_INCREMENT,
+        householdid int NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (householdid) REFERENCES household(id)
+);
+
+CREATE TABLE IF NOT EXISTS datet (
+        id int NOT NULL AUTO_INCREMENT,
+        dt datetime NOT NULL,
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS averagewindspeed (
+        i int NOT NULL AUTO_INCREMENT,
+	locationid int NOT NULL,
+	windspeed float NOT NULL,
+	dt date NOT NULL,
+	PRIMARY KEY (i),
+	FOREIGN KEY (locationid) REFERENCES location(id)
 );
 
 CREATE TABLE IF NOT EXISTS windspeed (
-	i INT NOT NULL AUTO_INCREMENT,
-	locationid INT NOT NULL,
-	windspeed FLOAT NOT NULL,
-	datetimeid INT NOT NULL,
+	i int NOT NULL AUTO_INCREMENT,
+	locationid int NOT NULL,
+	windspeed float NOT NULL,
+	datetimeid int NOT NULL,
 	PRIMARY KEY (i),
 	FOREIGN KEY (locationid) REFERENCES location(id),
 	FOREIGN KEY (datetimeid) REFERENCES datet(id)
 );
 
 CREATE TABLE IF NOT EXISTS powerusage (
-	i INT NOT NULL AUTO_INCREMENT,
-	householdid INT NOT NULL,
-	value FLOAT NOT NULL,
-	datetimeid INT NOT NULL,
+	i int NOT NULL AUTO_INCREMENT,
+	householdid int NOT NULL,
+	value float NOT NULL,
+	datetimeid int NOT NULL,
 	PRIMARY KEY (i),
-	FOREIGM KEY (householdid) REFERENCES household(id),
+	FOREIGN KEY (householdid) REFERENCES household(id),
 	FOREIGN KEY (datetimeid) REFERENCES datet(id)
 );
 
 CREATE TABLE IF NOT EXISTS powergenerated (
-	i INT NOT NULL AUTO_INCREMENT,
-	householdid INT NOT NULL,
-	value FLOAT NOT NULL,
-	datetimeid INT NOT NULL,
+	i int NOT NULL AUTO_INCREMENT,
+	householdid int NOT NULL,
+	value float NOT NULL,
+	datetimeid int NOT NULL,
 	PRIMARY KEY (i),
-	FOREIGN KEY (householdid) REFERENCES household(id)
+	FOREIGN KEY (householdid) REFERENCES household(id),
 	FOREIGN KEY (datetimeid) REFERENCES datet(id)
-);
-
-CREATE TABLE IF NOT EXISTS datet (
-	id INT NOT NULL AUTO_INCREMENT,
-	dt DATETIME NOT NULL,
-	PRIMARY KEY (id)
 );
