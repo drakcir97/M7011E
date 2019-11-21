133d247624327b9b062af2cbbcbef0c08086f4cb
mikala3 <mikala-3@student.ltu.se>
2019-11-21 14:28:04 +0100

    added wind,power,cost and more to the simulator

--- database/createtable.sql
+++ database/createtable.sql
@@ -1,39 +1,37 @@
 USE miri;
 
 CREATE TABLE IF NOT EXISTS location (
-        id int NOT NULL AUTO_INCREMENT,
-        name char(255) UNIQUE,
-        PRIMARY KEY (id)
+    id int NOT NULL AUTO_INCREMENT,
+    name char(255) UNIQUE,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS household (
 	id int NOT NULL AUTO_INCREMENT,
 	locationid int NOT NULL,
-	powerusageid int NOT NULL,
-	powergeneratedid int NOT NULL,
 	housetype char(255),
 	PRIMARY KEY (id),
 	FOREIGN KEY (locationid) REFERENCES location(id)
 );
 
 CREATE TABLE IF NOT EXISTS user (
-        id int NOT NULL AUTO_INCREMENT,
-        householdid int NOT NULL,
-        PRIMARY KEY (id),
-        FOREIGN KEY (householdid) REFERENCES household(id)
+    id int NOT NULL AUTO_INCREMENT,
+    householdid int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (householdid) REFERENCES household(id)
 );
 
 CREATE TABLE IF NOT EXISTS datet (
-        id int NOT NULL AUTO_INCREMENT,
-        dt datetime NOT NULL,
-        PRIMARY KEY (id)
+    id int NOT NULL AUTO_INCREMENT,
+    dt datetime NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS averagewindspeed (
-        i int NOT NULL AUTO_INCREMENT,
+    i int NOT NULL AUTO_INCREMENT,
 	locationid int NOT NULL,
 	windspeed float NOT NULL,
-	dt date NOT NULL,
+	dt date NOT NULL UNIQUE,
 	PRIMARY KEY (i),
 	FOREIGN KEY (locationid) REFERENCES location(id)
 );
@@ -44,6 +42,15 @@ CREATE TABLE IF NOT EXISTS windspeed (
 	windspeed float NOT NULL,
 	datetimeid int NOT NULL,
 	PRIMARY KEY (i),
+	FOREIGN KEY (locationid) REFERENCES location(id),
+	FOREIGN KEY (datetimeid) REFERENCES datet(id)
+);
+
+CREATE TABLE IF NOT EXISTS temperature (
+	i int NOT NULL AUTO_INCREMENT,
+	locationid int NOT NULL,
+	temperature float NOT NULL,
+	datetimeid int NOT NULL,
 	FOREIGN KEY (locationid) REFERENCES location(id),
 	FOREIGN KEY (datetimeid) REFERENCES datet(id)
 );
@@ -59,6 +66,25 @@ CREATE TABLE IF NOT EXISTS powerusage (
 );
 
 CREATE TABLE IF NOT EXISTS powergenerated (
+	i int NOT NULL AUTO_INCREMENT,
+	householdid int NOT NULL,
+	value float NOT NULL,
+	datetimeid int NOT NULL,
+	PRIMARY KEY (i),
+	FOREIGN KEY (householdid) REFERENCES household(id),
+	FOREIGN KEY (datetimeid) REFERENCES datet(id)
+);
+
+CREATE TABLE IF NOT EXISTS powertotal (
+	i int NOT NULL AUTO_INCREMENT,
+	powerin float NOT NULL,
+	powerout float NOT NULL,
+	datetimeid int NOT NULL,
+	PRIMARY KEY (i),
+	FOREIGN KEY (datetimeid) REFERENCES datet(id)
+);
+
+CREATE TABLE IF NOT EXISTS powercosthousehold (
 	i int NOT NULL AUTO_INCREMENT,
 	householdid int NOT NULL,
 	value float NOT NULL,
--- simulator/simulator.js
+++ simulator/simulator.js
@@ -5,6 +5,18 @@ const app = express()
 var randomNormal = require('random-normal')
 var mysql = require('mysql');
 
+//	var elePro = ((1.3968**windSpeedCurrentTime)*56.94).toFixed(3);
+var config = {
+	apartmentPower = 300, //Average power used by an apartment.
+	housePower = 2500, //Average power used by house.
+	tempMinAffect = 25, //At this value no power goes to heat.
+	tempMaxAffect = -30, //At this value maximum power goes to heat.
+	tempAffect = 2, //Maximum affect temperature has.
+	tempCoefficient = 0.6, //Procentage that is affected by temperature.
+	powerCostHigh = 0.01, //Cost if powerplant
+	powerCostLow = 0.005, //Cost if wind
+}
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -18,63 +30,270 @@ function getNormValues(meanIn, stIn){
 
 }
 
-function setValues(){
-	//get windspeed from database put it where 'windSpeedCurrentTime' is
-//	var curWind = randomNormal({mean: getPreviousvaluefromdatabase, st: });
-	var elePro = ((1.3968**windSpeedCurrentTime)*56.94).toFixed(3);
-        var eleConApart = randomNormal({mean: 1.25, dev: 0.35});
-        var eleConHouse = randomNormal({mean: 2.25, dev: 0.35});
-        request({
-                url: 'https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/159880/period/latest-hour/data.json',
-                json: true
-                }, function(error, response, body) {
-                var test = body;
-                var objVal = test.value;
-                var objName = test.station.name;
-                var objTemp = JSON.stringify(objVal[0].value);
-                res.send("this is the temperature in "+" "+objName+" "+objTemp+" right now");
-        });
-
+//Generates temperature, using smhi rest api
+function generateTemperature(location,dateid) {
+	const request = require('request')
+	request({
+		//url below has the id for Arvidsjaur
+		url: 'https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/159880/period/latest-hour/data.json',
+		json: true
+		}, function(error, response, body) {
+		var test = body;
+		var objVal = test.value;
+//		var location = test.station.name;
+		var temperature = (parseFloat(JSON.stringify(objVal[0].value).slice(1, -1)));
+		var sqlLocation = mysql.format("SELECT id FROM location WHERE name=?", [location]);
+		con.query(sqlLocation, function (err, result) {
+			if (err) throw err;           
+			//var nowDate = new Date(); 
+			//var date = nowDate.getFullYear()+'-'+(nowDate.getMonth()+1)+'-'+nowDate.getDate(); 
+			var locationId = result[0]['id'];
+			var sql = mysql.format("INSERT INTO temperature (locationid, temperature, datetimeid) VALUES (?,?,?)", [locationId, temperature, dateid]);
+			con.query(sql, function(err2, result) {
+				if (err2) throw err2;
+				console.log("Temperature was inserted");
+			});
+		});
+	});
+		
 }
 
-function generateWindForLocation(location){
+//Updates values in db. That is, generates new values and inserts them accordingly.
+function update() {
+	var location = "Kiruna";
+	var dateid, date = generateDate();
 	createLocation(location);
-	var sqlLocation = mysql.format("SELECT id FROM location WHERE name=?", [location]);
-	con.query(sqlLocation, function (err, result) {
-                if (err) throw err;
-		var meanWind = getNormValues(7, 2);
-		var nowDate = new Date(); 
-		var date = nowDate.getFullYear()+'-'+(nowDate.getMonth()+1)+'-'+nowDate.getDate(); 
-		var locationId = result[0]['id'];
-        	var sql = mysql.format("INSERT INTO averagewindspeed (locationid, windspeed, dt) VALUES (?,?,?)", [locationId, meanWind, date]);
-        	con.query(sql, function (err2, result) {
-                	if (err2) throw err2;
-                	console.log("1 record inserted");
-        	});
+	generateTemperature(location);
+	generateWindForDay(location,date);
+	generateWindForLocation(location,date,dateid);
+	var sqlHousehold = "SELECT id FROM household";
+	con.query(sqlInsert, function (err, result) {
+		for(house in result[0]['id']) {
+			generatePowerForTime(house,dateid);
+			generatePowerUsageForTime(house,dateid);
+		}
+	});
+	generatePowerTotal(dateid);
+	var totalin,totalout = getPowerTotal(dateid);
+	var sqlCountHousehold = "SELECT COUNT(id) FROM household";
+	con.query(sqlCountHouseHold, function (err, result) {
+		var sqlHousehold = "SELECT id FROM household";
+		var totalhouseholds = result[0]['COUNT(id)'];
+		con.query(sqlHouseHold, function (err, result) {
+			for(house in result[0]['id']) {
+				generatePowerCost(house, dateid,totalin,totalout,totalhouseholds);
+			}
+		});
 	});
 }
 
+//Generates a date object in db, used to update values. Returns two dates, one with time, one without.
+function generateDate() {
+	var dateTime = require('node-datetime');
+	var dt = dateTime.create();
+	var nowDate = new Date(); 
+	var date = nowDate.getFullYear()+'-'+(nowDate.getMonth()+1)+'-'+nowDate.getDate(); 
+	var formatted = dt.format('Y-m-d H:M:S');
+	var sqlInsert = mysql.format("INSERT INTO datet (dt) VALUES (?)", [formatted]);
+	con.query(sqlInsert, function (err, result) {
+		if (err) throw err;
+		console.log("date was inserted");
+		var sqlLookup = mysql.format("Select id FROM datet WHERE dt=?", [formatted]);
+		con.query(sqlLookup, function (err, result) {
+			if (err) throw err;
+			return result[0]['id'],date;
+	});
+	
+}
+
+//Tests if wind for day and location already exists, return true if it does.
+function testWindForDay(location,date) {
+	var sql = mysql.format("SELECT COUNT(id) FROM location WHERE name=? AND dt=?", [location,date]);
+	con.query(sqlLocation, function (err, result) {
+		if (err) throw err;
+		var count = result[0]['COUNT(id)'];
+		if (count == 0) {
+			return false;
+		}
+		return true;
+	});
+}
+
+//Generates average wind for date for location if it does not exist.
+function generateWindForDay(location,date){
+	//createLocation(location);
+	if (testWindForDay(location,date)==false) {
+		var sqlLocation = mysql.format("SELECT id FROM location WHERE name=?", [location]);
+		con.query(sqlLocation, function (err, result) {
+        	if (err) throw err;
+			var meanWind = getNormValues(7, 2);
+			var locationId = result[0]['id'];
+        	var sql = mysql.format("INSERT INTO averagewindspeed (locationid, windspeed, dt) VALUES (?,?,?)", [locationId, meanWind, date]);
+    		con.query(sql, function (err2, result) {
+            	if (err2) throw err2;
+            	console.log("1 record inserted");
+			});
+			return date;
+		});
+	}
+}
+
+//Generate wind for location using average for that day.
+function generateWindForTime(location,date,dateid) {
+	var sqlLocation = mysql.format("SELECT id FROM location WHERE name=?", [location]);
+	con.query(sqlLocation, function (err, result) {
+		if (err) throw err;
+		var locationId = result[0]['id'];
+		var sqlGetAvg = mysql.format("SELECT windspeed FROM averagewindspeed WHERE dt=? AND locationid=?", [date,locationId]);
+		con.query(sqlGetAvg, function(err, result) {
+			if (err) throw err;
+			var avgForDay = result[0]['windspeed'];
+			var meanWind = getNormValues(avgForDay,2); //Replace later for real deviation.
+			var sql = mysql.format("INSERT INTO windspeed (locationid, windspeed, datetimeid) VALUES (?,?,?)", [locationId,meanWind,dateid]);
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+			});
+		});
+	});
+}
+
+//Generates power generated for household using windspeed.
+function generatePowerForTime(householdid,dateid) {
+	var sqlType = mysql.format("SELECT windspeed FROM windspeed WHERE datetimeid=?", [dateid]);
+	con.query(sqlLocation, function (err, result) {
+		if (err) throw err;
+		var windSpeedCurrentTime = result[0]['windspeed'];
+		var meanPwr = ((1.3968**windSpeedCurrentTime)*56.94).toFixed(3);
+		var sqlInsert = mysql.format("INSERT INTO powergenerated (householdid, value, datetimeid) VALUES (?,?,?)", [householdid,meanPwr,dateid]);
+		con.query(sqlInsert, function(err, result) {
+			if (err) throw err;
+		});
+	});
+}
+
+//Generates power used using temperature and values from config.
+function generatePowerUsageForTime(householdid,dateid) {
+	var sqlType = mysql.format("SELECT housetype FROM household WHERE id=?", [householdid]);
+	con.query(sqlLocation, function (err, result) {
+		if (err) throw err;
+		var housetype = result[0]['housetype'];
+		var meanPwr = getNormValues(typePwr,typePwr*0.2);
+		var sqlTemp = mysql.format("SELECT temperature FROM temperature WHERE datetimeid=?", [dateid]);
+		con.query(sqlTemp, function(err, result) {
+			if (err) throw err;
+			var temp = result[0]['temperature'];
+			var typePwr = 0;
+			if (housetype == "apartment") {
+				typePwr = config.apartmentPower;
+			} else {
+				typePwr = config.housePower;
+			}
+			var tempPwr = 0;
+			if (temp > config.tempMinAffect) { //Temp is higher then minumum affect, 0 goes to heating.
+				tempPwr = 0;
+			} else if (temp < config.tempMaxAffect) { //Temp is lower then max affect, max to heating.
+				tempPwr = typePwr*config.tempCoefficient*config.tempAffect; 
+			} else { //Somewhere inbetween, calculate amount using linear division of affect.
+				tempPwr = typePwr*config.tempCoefficient*temp*(config.tempAffect/(Math.abs(config.tempMaxAffect)+Math.abs(config.tempMinAffect)))
+			}
+			var pwr = (1-config.tempCoefficient) * typePwr + tempPwr;
+			var sqlInsert = mysql.format("INSERT INTO powerusage (householdid, value, datetimeid) VALUES (?,?,?)", [householdid,pwr,dateid]);
+			con.query(sqlInsert, function(err, result) {
+				if (err) throw err;
+			});
+		});
+	});
+}
+
+//Creates location if it does not exist.
 function createLocation(location){
 	var sql = mysql.format("SELECT COUNT(*) FROM location WHERE name=?", [location]);  
-        con.query(sql, function (err, result) {
-            	if (err) throw err;           
+    con.query(sql, function (err, result) {
+        if (err) throw err;           
 		console.log("location was found");
 		var count = result[0]['COUNT(*)'];
 		if (count == 0) {
-                	var sql = mysql.format("INSERT INTO location (name) VALUES (?)", [location]);
+        	var sql = mysql.format("INSERT INTO location (name) VALUES (?)", [location]);
 			console.log("after");
-                	con.query(sql, function(err2, result) {
-                        	if (err2) throw err2;
-                        	console.log("Location not found, was inserted");
-                	});
+            con.query(sql, function(err2, result) {
+                if (err2) throw err2;
+                console.log("Location not found, was inserted");
+            });
 		}
-       	});
+    });
+}
+
+function getPowerTotal(dateid) {
+	var sql = mysql.format("SELECT powerin,powerout FROM powertotal WHERE datetimeid=?", [dateid]);
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		var totalin = result[0]['powerin'];
+		var totalout = result[0]['powerout'];
+		return totalin,totalout;
+	});
+}
+
+function generatePowerCost(householdid, dateid, totalin, totalout,totalhouseholds) {
+	var powergenerated = 0;
+	var powerusage = 0;
+	var powersum = 0;
+	var powercost = 0;
+	var sql = mysql("SELECT value FROM powergenerated WHERE householdid=?", [householdid]);
+	con.query(sql, function (err, result) {
+		powergenerated = parseFloat(result[0]['value']);
+		var sql = mysql("SELECT value FROM powerusage WHERE householdid=?", [householdid]);
+		con.query(sql, function (err, result) {
+			powerusage = parseFloat(result[0]['value']);
+			powersum = powergenerated - powerusage;
+			if(powersum >= 0) {
+				powercost = config.powerCostLow*powersum;
+			} else {
+				if (totalin>totalout) {
+					powercost = -config.powerCostLow*powersum;
+				} else {
+					if ((powersum + totalin/totalhouseholds) > 0) {
+						powercost = config.powerCostLow*powersum;
+					} else {
+						var powerCheap = totalin/totalhouseholds;
+						var powerExpensive = (powersum+powerCheap);
+						powercost = powerExpensive*config.powerCostHigh - powerCheap*config.powerCostLow;
+					}
+				}
+			} 
+		}
+	}
+	
+}
+
+function generatePowerTotal(dateid) {
+//	(parseFloat(JSON.stringify(objVal[0].value)));
+	var totalgenerated = 0;
+	var totalused = 0;
+	var sql = mysql.format("SELECT value FROM powergenerated WHERE datetimeid=?", [dateid]);
+	con.query(sql, function (err, result) {
+		if (err) throw err;  
+		for(val in result[0]['value']) {
+			totalgenerated = totalgenerated + parseFloat(val);
+		}
+		var sql = mysql.format("SELECT value FROM powerusage WHERE datetimeid=?", [dateid]);
+		con.query(sql, function (err, result) {
+			if (err) throw err;  
+			for(val in result[0]['value']) {
+				totalused = totalused + parseFloat(val);
+			}
+			var sql = mysql.format("INSERT INTO powertotal (powerin,powerout,datetimeid) VALUES (?)", [totalgenerated, totalused, dateid]);
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+				console.log("Total power inserted");
+			});
+		});
+	});
 }
 
 con.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected to db");
-	generateWindForLocation("Kiruna");
+	update();
 });
 
 //con.end(function(err) {
