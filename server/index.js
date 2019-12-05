f41a41690febe5636b57ed5af099bd5871f6e0ec
mikala3 <mikala-3@student.ltu.se>
2019-12-05 14:07:39 +0100

    added signup page

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var https = require('https')
 var request = require('path')
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var mysql = require('mysql');
 'use strict';
 var crypto = require('crypto')
 
@@ -11,6 +12,13 @@ var options = {
         key: fs.readFileSync('key.pem'),
         cert: fs.readFileSync('cert.pem')
 };
+
+var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "serverdb"
+      });
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
@@ -49,8 +57,43 @@ app.get('/', (req, res) => {
 
 app.post('/login', function(req, res) {
         //res.send('you sent the name "' + req.body.username + '".');
-        var testEncrypt = saltHashPassword(req.body.userpassword);
-        res.send("salt: "+testEncrypt.salt+" hash: "+testEncrypt.passwordHash);
+//        var testEncrypt = saltHashPassword(req.body.userpassword);
+//        res.send("salt: "+testEncrypt.salt+" hash: "+testEncrypt.passwordHash);
+        var sqlLogin = mysql.format("SELECT id FROM user WHERE email=?", [req.body.emailaddress]);
+        con.query(sqlLogin, function(err, result) {
+                if(err) throw err;
+                var userid = result[0]['id'];
+                var sqlUserId = mysql.format("SELECT pw FROM passwords WHERE userid=?", [userid]);
+                con.query(sqlUserId, function(err, result){
+                        var pw = result[0]['pw'];
+                        if (pw == req.body.userpassword) {
+                                res.sendFile('home.html', {root : '/'});
+                        }
+                });
+        });
+});
+
+app.get('/signup', (req, res) => {
+        res.sendFile('signup.html', {root : '/'});
+        req.body.emailaddress;
+        req.body.name;
+        req.body.userpassword;
+        var sqlSignup = mysql.format("INSERT INTO user (name,email) VALUES (?,?)", [req.body.name,req.body.emailaddress]);
+        con.query(sqlSignup, function(err,result) {
+                if (err) throw err;
+                var sqlUserId = mysql.format("SELECT id FROM user WHERE name=? AND email=?", [req.body.name,req.body.emailaddress]);
+                con.query(sqlUserId, function(err,result) {
+                        var userid = result[0]['id'];
+                        var sqlPassword = mysql.format("INSERT INTO passwords (userid,pw) VALUES (?,?)", [userid, req.body.userpassword]);
+                        con.query(sqlPassword, function(err, result) {
+                                if (err) throw err;   
+                        });
+                });
+        });
+});
+
+app.get('/home', (req, res) => {
+        res.sendFile('home.html', {root : '/'});
 });
 
 //https.createServer(options, function (req, res) {
