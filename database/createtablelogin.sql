a1ad5f71f485561bfc4455843d5d8cee67bdea7a
mikala3 <mikala-3@student.ltu.se>
2019-12-05 14:14:54 +0100

    added signup page

--- database/createtablelogin.sql
+++ database/createtablelogin.sql
@@ -1,14 +1,14 @@
 USE serverdb;
 
 CREATE TABLE IF NOT EXISTS user (
-    id int NOT NULL AUTO_INCREMENET,
+    id int NOT NULL AUTO_INCREMENT,
     name char(255) NOT NULL,
     email char(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS passwords (
-    id int NOT NULL AUTO_INCREMENET,
+    id int NOT NULL AUTO_INCREMENT,
     userid int NOT NULL UNIQUE,
     pw char(255),
     PRIMARY KEY (id),
