de251cb873ecd72086029d43bef770595ee882bc
drakcir97 <richol-6@student.ltu.se>
2019-12-09 14:52:38 +0100

    syntax

--- authentication/authcontrol.js
+++ authentication/authcontrol.js
@@ -5,7 +5,7 @@ const app = express()
 module.exports = {
     'secret': 'supersecret',
     register: function(userid) {
-        var token = jwt.sign({ id: userid }, config.secret, {
+        var token = jwt.sign({ id: userid }, this.secret, {
             expiresIn: 86400 // expires in 24 hours
         });
         return token;
