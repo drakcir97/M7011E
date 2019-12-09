b87bb9e4515d5df738f3a659000fd7c737ee7df9
drakcir97 <richol-6@student.ltu.se>
2019-12-09 14:45:28 +0100

    syntax

--- server/index.js
+++ server/index.js
@@ -175,7 +175,7 @@ app.post('/signup', function(req, res) {
 app.get('/home', (req, res) => {
         var token = req.headers['x-access-token'];
         if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
-        jwt.verify(token, config.secret, function(err, decoded) {
+        jwt.verify(token, authenticator.secret, function(err, decoded) {
                 if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
                 
                 res.status(200).send(decoded);
