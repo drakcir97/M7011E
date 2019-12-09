641419155e039fb48b6106ac4903ee65ad1b4051
drakcir97 <richol-6@student.ltu.se>
2019-12-09 14:39:33 +0100

    Image and captcha

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ var fs = require('fs');
 var mysql = require('mysql');
 var authenticator = require("../authentication/authcontrol");
 'use strict';
-var crypto = require('crypto')
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var options = {
         key: fs.readFileSync('key.pem'),
@@ -19,7 +20,23 @@ var con = mysql.createConnection({
         user: "root",
         password: "password",
         database: "serverdb"
-      });
+});
+
+var mime = {
+        html: 'text/html',
+        txt: 'text/plain',
+        css: 'text/css',
+        gif: 'image/gif',
+        jpg: 'image/jpeg',
+        png: 'image/png',
+        svg: 'image/svg+xml',
+        js: 'application/javascript'
+};
+
+var captcha = ["SNAKE","JSON","CAPTCHA","PASSWORD","TEST","ANSWER"];
+
+
+var dir = path.join(__dirname, 'images/captcha');
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
@@ -75,16 +92,32 @@ app.post('/login', function(req, res) {
                         var saltedpw = saltHashPassword(req.body.userpassword,salt);
                         if (pw == saltedpw.passwordHash) {
                                 var token = authenticator.register(userid);
-                                res.redirect('/home');
+                                res.status(200).send({ auth: true, token: token })
+                                //res.redirect('/home');
                         } else {
-                                res.redirect(404,'/');
+                                return res.status(401).send({ auth: false, token: null });
+                                //res.redirect(404,'/');
                         }
                 });
         }); 
 });
 
 app.get('/signup', (req, res) => {
-        res.sendFile('signup.html', {root : './'});
+        var file = path.join(dir, req.path.replace(/\/$/, '/signup.html'));
+        if (file.indexOf(dir + path.sep) !== 0) {
+                return res.status(403).end('Forbidden');
+        }
+        var type = mime[path.extname(file).slice(1)] || 'text/plain';
+        var s = fs.createReadStream(file);
+        s.on('open', function () {
+                res.set('Content-Type', type);
+                s.pipe(res);
+        });
+        s.on('error', function () {
+                res.set('Content-Type', 'text/plain');
+                res.status(404).end('Not found');
+        });
+        //res.sendFile('signup.html', {root : './'});
         //req.body.emailaddress;
         //req.body.name;
         //req.body.userpassword;
@@ -97,6 +130,26 @@ app.get('/signout', (req, res) => {
         //req.body.name;
         //req.body.userpassword;
         
+});
+
+app.get('/userpage', (req, res) => {
+     //   var sqlSelectPicture = mysql.format("SELECT picture FROM user WHERE id=?")
+      //  res.sendFile('user.html', {root : './'});
+        //req.body.emailaddress;
+        //req.body.name;
+        //req.body.userpassword;
+        
+});
+
+app.post('/addPicture', (req, res) {
+        var sqlInsertPicture = mysql.format("INSERT INTO user (picture) VALUES (?)", [req.body.picture]);
+        con.query(sqlInsertPicture, function(err,result) {
+                if(err){
+                        //??
+                } else {
+                        //??
+                }
+        });
 });
 
 app.post('/signup', function(req, res) {
@@ -120,6 +173,13 @@ app.post('/signup', function(req, res) {
 });
 
 app.get('/home', (req, res) => {
+        var token = req.headers['x-access-token'];
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+        jwt.verify(token, config.secret, function(err, decoded) {
+                if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+                
+                res.status(200).send(decoded);
+        });
         res.sendFile('home.html', {root : './'});
 });
 
