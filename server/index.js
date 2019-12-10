f7c73497880302f28b03611ef18c319184c218cb
drakcir97 <richol-6@student.ltu.se>
2019-12-10 16:57:25 +0100

    picture test

--- server/index.js
+++ server/index.js
@@ -106,22 +106,22 @@ app.post('/login', function(req, res) {
 });
 
 app.get('/signup', (req, res) => {
-        var file = path.join(dir, req.path.replace(/\/$/, '/signup.html'));
-        if (file.indexOf(dir + path.sep) !== 0) {
-                return res.status(403).end('Forbidden');
-        }
-        var type = mime[path.extname(file).slice(1)] || 'text/plain';
-        console.log(type,"\n");
-        var s = fs.createReadStream(file);
-        s.on('open', function () {
-                res.set('Content-Type', type);
-                s.pipe(res);
-        });
-        s.on('error', function () {
-                res.set('Content-Type', 'text/plain');
-                res.status(404).end('Not found');
-        });
-        //res.sendFile('signup.html', {root : './'});
+        // var file = path.join(dir, req.path.replace(/\/$/, '/signup.html'));
+        // if (file.indexOf(dir + path.sep) !== 0) {
+        //         return res.status(403).end('Forbidden');
+        // }
+        // var type = mime[path.extname(file).slice(1)] || 'text/plain';
+        // console.log(type,"\n");
+        // var s = fs.createReadStream(file);
+        // s.on('open', function () {
+        //         res.set('Content-Type', type);
+        //         s.pipe(res);
+        // });
+        // s.on('error', function () {
+        //         res.set('Content-Type', 'text/plain');
+        //         res.status(404).end('Not found');
+        // });
+        res.sendFile('signup.html', {root : './'});
         //req.body.emailaddress;
         //req.body.name;
         //req.body.userpassword;
