66da0a03f717db0d25c61fb0d938259b8b6e25c3
mikala3 <mikala-3@student.ltu.se>
2019-12-11 23:38:20 +0100

    changed img src

--- server/index.js
+++ server/index.js
@@ -203,6 +203,7 @@ app.post('/addPicture', function(req, res) {
 
 app.post('/signup', function(req, res) {
         console.log(req.body.usercaptcha); //test captcha
+        console.log(req.body.path);
         var sqlSignup = mysql.format("INSERT INTO user (name,email) VALUES (?,?)", [req.body.name,req.body.emailaddress]);
         con.query(sqlSignup, function(err,result) {
                 if (err){
