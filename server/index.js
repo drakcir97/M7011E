1790d7f8ff681a2f68f76ee364b3f5160d9bea99
drakcir97 <richol-6@student.ltu.se>
2019-12-12 13:05:30 +0100

    Upload image of house

--- server/index.js
+++ server/index.js
@@ -194,7 +194,7 @@ app.post('/addPicture', function(req, res) {
                 if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
                 
                 //res.status(200).send(decoded);
-                var sqlInsertPicture = mysql.format("INSERT INTO picture (userid,picture) VALUES (?,?)", [JSON.stringify(decoded.id),req.body.picture]);
+                var sqlInsertPicture = mysql.format("INSERT INTO picture (userid,picture) VALUES (?,?)", [JSON.stringify(decoded.id),req.body.house]);
                 con.query(sqlInsertPicture, function(err,result) {
                         if(err){
                                 //??
