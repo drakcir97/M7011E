395ead5209d271caba61169060ed9ba9606fac7c
mikala3 <mikala-3@student.ltu.se>
2019-12-17 01:16:57 +0100

    try implementing onlinestatus

--- server/index.js
+++ server/index.js
@@ -400,6 +400,7 @@ io.on('connection', function(socket){
         console.log('a user connected');
         socket.on('chat message', function(msg){
                 console.log('message: ' + msg);
+        });
         socket.on('disconnect', function(){
                 console.log('user disconnected');
         });
