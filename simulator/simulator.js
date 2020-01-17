a7c17eb4d84153fa4e650ae3f10020e7f7a5de72
mikala3 <mikala-3@student.ltu.se>
2020-01-17 16:11:16 +0100

    try display info from the api

--- simulator/simulator.js
+++ simulator/simulator.js
@@ -434,7 +434,7 @@ async function generatePowerCost(householdid, dateid, totalin, totalout,totalhou
 	con.query(sqlSettingsCheck, async function(err, result) {
 		var count = result[0]['COUNT(simulationsettings.ratiokeep'];
 		console.log("Count in powercost: "+count);
-		if (count != "") {
+		if (count) {
 			var sqlSettings = mysql.format("SELECT simulationsettings.ratiokeep,simulationsettings.ratiosell FROM simulationsettings INNER JOIN user ON simulationsettings.userid=user.id WHERE user.householdid=?", [householdid]);
 			con.query(sqlSettings, async function(err,result) {
 				var ratiokeep = result[0]['simulationsettings.ratiokeep'];
