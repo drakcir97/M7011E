8dc62ce81909ad5a89c36e9653f1dbbd97abdc49
mikala3 <mikala-3@student.ltu.se>
2020-01-17 14:13:58 +0100

    try display info from the api

--- server/index.js
+++ server/index.js
@@ -782,7 +782,7 @@ app.get('/api/:inp', (req, res) => {
                 socket.on('response', function (message) { 
                         console.log("before socket.emit,  req.params.inp = "+req.params.inp+"  id: decoded.id = "+decoded.id)
                         console.log("after first emit")
-                        return socket.emit(inp, {id: decoded.id}); //Send id to api.
+                        socket.emit(inp, {id: decoded.id}); //Send id to api.
                         console.log(message);
                 });
                 
