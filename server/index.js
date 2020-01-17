88f27a99d9fdbc9256cf238f02ae858f6c58ca34
mikala3 <mikala-3@student.ltu.se>
2020-01-17 18:21:32 +0100

    try display info from the api

--- server/index.js
+++ server/index.js
@@ -665,14 +665,14 @@ app.post('/settings', function(req, res) {
                 var ratiokeep = req.body.ratiokeep;
                 var ratiosell = req.body.ratiosell;
                 var sqlSettingsCount = mysql.format("SELECT COUNT(userid) FROM simulationsettings WHERE userid=?", [decoded.id]);
-                conn.query(sqlSettingsCount, (err, results) => {
+                con.query(sqlSettingsCount, (err, results) => {
                         if (err) {
                                 console.log(err);
                         } else {
                                 var count = parseInt(JSON.stringify(results[0]['COUNT(userid)']));
                                 if (count == 0) {
                                         var sqlSettings = mysql.format("INSERT INTO simulationsettings (userid, ratiokeep, ratiosell) VALUES (?,?,?)", [decoded.id,ratiokeep,ratiosell]);
-                                        conn.query(sqlSettings, (err, results) => {
+                                        con.query(sqlSettings, (err, results) => {
                                                 if (err) {
                                                         console.log(err);
                                                 } else {
@@ -681,7 +681,7 @@ app.post('/settings', function(req, res) {
                                         });
                                 } else {
                                         var sqlSettings = mysql.format("UPDATE simulationsettings SET ratiokeep=?,ratiosell=? WHERE userid=?", [ratiokeep,ratiosell,id]);
-                                        conn.query(sqlSettings, (err, results) => {
+                                        con.query(sqlSettings, (err, results) => {
                                                 if (err) {
                                                         console.log(err);
                                                 } else {
