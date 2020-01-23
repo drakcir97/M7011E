b19a29d12fb4ea39dc2afdec6ec9f95cc298ab65
drakcir97 <richol-6@student.ltu.se>
2020-01-23 15:30:33 +0100

    Changed time in token

--- authentication/authcontrol.js
+++ authentication/authcontrol.js
@@ -6,7 +6,7 @@ module.exports = {
     'secret': 'supersecret',
     register: function(userid,admin) {
         var token = jwt.sign({ id: userid, admin: admin }, this.secret, {
-            expiresIn: 7200000 // expires in 2 hours
+            expiresIn: '12h' // expires in 12 hours
         });
         return token;
     },
