b36fbd4c080185f5f78470c29c40b52e1b5f0edb
drakcir97 <richol-6@student.ltu.se>
2020-01-28 12:44:36 +0100

    API powercost

--- restful-api/index.js
+++ restful-api/index.js
@@ -397,6 +397,7 @@ io.sockets.on('connect', function(socket)
                         console.log(err);
                     }
                     var plantid = results[0]['powerplant.id'];
+                    console.log("powerplantid"+plantid);
                     if (count == 0) {  
                         var sqlSettings = mysql.format("INSERT INTO powerplantsettings (powerplantid, powerCostHigh, powerCostLow) VALUES (?,?,?)", [plantid,high,low]);
                         conn.query(sqlSettings, (err, results) => {
