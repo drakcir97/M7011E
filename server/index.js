27130180df59972a1da2e13804c73f87318b77f2
drakcir97 <richol-6@student.ltu.se>
2020-01-29 15:43:55 +0100

    Syntax

--- server/index.js
+++ server/index.js
@@ -511,7 +511,7 @@ app.post('/blockusers', function(req, res) {
                                 //socket.emit('api/users');
                                 console.log(message);
                                 var parsed = JSON.parse(message);
-                                timeblocked = BigInt(parsed.response[0].dt);
+                                timeblocked = BigInt(parsed['response']['dt']);
 
                                 if (timeblocked == -1) {
                                         return res.redirect('/blockusers')
