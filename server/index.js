4f78a8ebd5e29b143230706390e72f536861a401
drakcir97 <richol-6@student.ltu.se>
2020-02-21 16:28:51 +0100

    Removed https

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var https = require('https')
+var http = require('http');
 var path = require('path')
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
